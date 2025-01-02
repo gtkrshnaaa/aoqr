daacbf0e0b4881a424859c5e470e3c53a1f503bb
gtkrshnaaa <gtkrishnacbspace@gmail.com>
2025-01-02 11:22:22 +0700

    🌱 feat: update file.

--- frontend/app/admin/objects/create/page.jsx
+++ frontend/app/admin/objects/create/page.jsx
@@ -246,7 +246,7 @@ const CreateObjectPage = () => {
             <button
               type="submit"
               form="modalForm"
-              className="bg-[#AAA577] text-white px-4 py-2 rounded hover:bg-blue-600"
+              className="bg-[#AAA577] text-white px-4 py-2 rounded"
             >
               Save Changes
             </button>
